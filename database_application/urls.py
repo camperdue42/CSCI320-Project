2089f92dd0988f4a6b85e23e8c3d3cbec092dcf9
Nikki Aldridge <ena9046@gmail.com>
2020-10-19 23:53:58 -0400

    making user view
    the view shows the heading, but no table content. not connected to db

--- database_application/urls.py
+++ database_application/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from database_application.views import index
+from database_application.views import users
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('users/', users, name='users'),
 ]
--- database_application/views.py
+++ database_application/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Context, loader
 
+from .models import user
+
 def index(request):
     template = loader.get_template("database_application/home.html")
     context = {}
+    return HttpResponse(template.render(context, request))
+
+def users(request):
+    usertable = user.objects.all()
+    template = loader.get_template("database_application/table.html")
+    context={}
     return HttpResponse(template.render(context, request))
