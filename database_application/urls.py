7c57675a2ddc6137bbf450156dfcf51c489f651e
Cam <camperdue42@gmail.com>
2020-10-22 02:04:08 -0400

    ^

--- database_application/urls.py
+++ database_application/urls.py
@@ -5,5 +5,5 @@ from database_application.views import index, user_table
 
 urlpatterns = [
     path('', index, name='index'),
-    path('users/', user_table, name='user_table'),
+    path('users', user_table, name='user_table'),
 ]
--- database_application/views.py
+++ database_application/views.py
@@ -9,8 +9,14 @@ def index(request):
     context = {}
     return HttpResponse(template.render(context, request))
 
+
 def user_table(request):
     usertable = User.objects.all()
     template = loader.get_template("database_application/table.html")
     context={'usertable':usertable,}
     return HttpResponse(template.render(context, request))
+
+
+def query(request):
+    #query = request.POST[input]
+    return user_table(request)
