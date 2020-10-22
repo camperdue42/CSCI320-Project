19291b852214bc346d4731c6a29c2bafd65a7ada
Nikki Aldridge <ena9046@gmail.com>
2020-10-22 02:32:57 -0400

    fix zone
    no more dictionaries = no more pain

--- database_application/models.py
+++ database_application/models.py
@@ -1,13 +1,22 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.contrib.postgres.fields import HStoreField
 
 #reference: https://docs.djangoproject.com/en/3.1/ref/models/fields/#model-field-types
 #PostgreSQL reference: https://docs.djangoproject.com/en/3.1/ref/contrib/postgres/fields/
 
 # Create your models here.
+class Utensil(models.Model):
+	UtensilName = models.CharField(max_length=100, primary_key=True)
+
+class StorageLocation(models.Model):
+	LocationID = models.PositiveIntegerField(primary_key=True)
+	LocationType = models.BooleanField()
+
 class User(models.Model):
-	UID = models.PositiveIntegerField(primary_key=True) #primary_key=True implies null=False and unique=True
+	UID = models.PositiveIntegerField(primary_key=True, default=0) #primary_key=True implies null=False and unique=True
 	Name = models.CharField(max_length=100)
-	StorageLocations = models.ForeignKey(StorageLocation, on_delete=models.CASCADE) #not too sure about this one
+	StorageLocations = models.ForeignKey(StorageLocation, on_delete=models.CASCADE, null=True) #not too sure about this one
 
 class Recipe(models.Model):
 	RecipeName = models.CharField(max_length=100, primary_key=True)
@@ -19,25 +28,20 @@ class Recipe(models.Model):
 	#PreviouslyMade is derived from DatesMade relationship attribute
 
 class Makes(models.Model):
+	User = models.ForeignKey(User, on_delete=models.CASCADE)
+	Recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
 	DatesMade = ArrayField(
 		models.DateField(auto_now_add=False, auto_now=False) #dates will not be automatically added on creation or update. Manually add data
 	)
 
-class Step(models.Model):
-	StepNumber = models.PositiveIntegerField(primary_key=True)
-	QuantitiesOfIngredients = HStoreField() #PostgreSQL specific field, functions like dict. see ref
-	RecipeName = models.ForeignKey(Recipe, on_delete=models.CASCADE) #many to one w/ recipe
-	Instructions = models.TextField(max_length=400)
-	Ingredients = models.ManyToManyField(Ingredient) #many to many w/ Ingredient
-
 class Ingredient(models.Model):
 	IngredientName = models.CharField(max_length=100, primary_key=True)
 	AmountLeft = models.PositiveIntegerField()
-	StorageLocation = models.ForeignKey(StorageLocation, on_delete=models.CASCADE) #not too sure about this one
+	StorageLocations = models.ForeignKey(StorageLocation, on_delete=models.CASCADE) #not too sure about this one
 
-class StorageLocation(models.Model):
-	LocationID = models.PositiveIntegerField(primary_key=True)
-	LocationType = models.BooleanField()
-
-class Utensil(models.Model):
-	UtensilName = models.CharField(max_length=100, primary_key=True)
+class Step(models.Model):
+	StepNumber = models.PositiveIntegerField(primary_key=True)
+	#QuantitiesOfIngredients = HStoreField() #PostgreSQL specific field, functions like dict. see ref
+	RecipeName = models.ForeignKey(Recipe, on_delete=models.CASCADE) #many to one w/ recipe
+	Instructions = models.TextField(max_length=400)
+	Ingredients = models.ManyToManyField(Ingredient) #many to many w/ Ingredient
