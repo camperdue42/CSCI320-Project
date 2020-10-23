a7cce961f7b229d7bfc82e25c1f1afeb0f06635a
Cam <camperdue42@gmail.com>
2020-10-22 21:50:12 -0400

    some more query stuff and add_to_database which is untested because I can't connect to the database

--- database_application/views.py
+++ database_application/views.py
@@ -21,15 +21,22 @@ def return_home(request, context):
     return HttpResponse(template.render(context, request))
 
 
+def add_to_database(array):
+    if array[1].casefold() == "Utensil".casefold():
+        Utensil.objects.create(UtensilName = " ".join(array[2:]))
+
+
 def query(request):
-    query = request.POST[input]
+    query = request.POST.get('input', ' ')
     query_as_array = query.split(' ')
     if query_as_array[0] == "users":
         return user_table(request)
     elif query_as_array[0].casefold() == "Help".casefold():
         context = {'extra_stuff':'insert help text here'}
         return return_home(request, context)
-    
+    elif query_as_array[0].casefold() == "Add".casefold():
+        context = {'extra_stuff':' '}
+        return return_home(request, context)
     else:
-        context = {'extra_stuff':'invalid command'}
+        context = {'extra_stuff':'Invalid Command!'}
         return return_home(request, context)    
